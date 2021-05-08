2041b42e4ba9e79762d25153ee4f918c10342c83
Matheus Alencar <matheus@kleros.io>
2021-05-08 17:09:05 -0300

    feat: implement payment status endpoint

--- src/controllers/payment-status.ts
+++ src/controllers/payment-status.ts
@@ -1,33 +1,26 @@
 import { Request, RequestHandler, Response } from 'express';
-import Joi from '@hapi/joi';
 
-import requestMiddleware from '../middleware/request-middleware';
 import logger from '../logger';
-
-const paymentStatusSchema = Joi.object().keys({});
+import { PaymentRequest } from '../models';
 
 export default function buildPaymentStatusController(): RequestHandler {
-  return requestMiddleware(
-    async function paymentStatusController(
-      req: Request,
-      res: Response,
-    ): Promise<void> {
-      try {
-        // TODO: Fetch payment request status and return.
-
-        res.status(200).send();
-      } catch (error) {
-        logger.error({
-          level: 'error',
-          message: `PaymentStatusController: Could not fetch payment status.`,
-          error,
-        });
-        res.status(500).json({
-          error,
-          message: 'Error: (Please notify at vago.visus@pm.me)',
-        });
-      }
-    },
-    { validation: { body: paymentStatusSchema } },
-  );
+  return async function paymentStatusController(
+    req: Request,
+    res: Response,
+  ): Promise<void> {
+    try {
+      const { id } = req.query;
+      res.status(200).json(await PaymentRequest.findById(id));
+    } catch (error) {
+      logger.error({
+        level: 'error',
+        message: `PaymentStatusController: Could not fetch payment status.`,
+        error,
+      });
+      res.status(500).json({
+        error,
+        message: 'Error: (Please notify at vago.visus@pm.me)',
+      });
+    }
+  };
 }
